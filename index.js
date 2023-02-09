c36b9ca5ce3dc4f407c244899c8a4fef855c8846
Jethro <jethrotiago@hotmail.com>
2023-02-09 13:18:56 -0300

    Realizado testes de função

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ class Cliente {
 class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        let valorSacado = 200;
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor > 0) {                 //evita que depositem valor negativo
+            this.saldo += valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
@@ -20,5 +33,12 @@ const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.saldo = 0;
 
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.depositar(50);
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(0);
+
+console.log(contaCorrenteRicardo.saldo);
 console.log(cliente1);
 console.log(cliente2);
