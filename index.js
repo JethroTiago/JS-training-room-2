bda34523f6547ab802bd340e4e2f022479153601
Jethro <jethrotiago@hotmail.com>
2023-02-11 22:50:04 -0300

    Criado Classe ContaCorrente.js

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-class Cliente {
-    nome;
-    cpf;
+sacar(valor) {
+    let valorSacado = 200;
+    if (this._saldo >= valor) {
+        this._saldo -= valor;
+        return valor;
+    }
 }
 
-class ContaCorrente {
-    agencia;
-    _saldo;
-
-    sacar(valor) {
-        let valorSacado = 200;
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
+depositar(valor) {
+    if (valor <= 0) {
+        return;
     }
-
-    depositar(valor) { 
-        if(valor <= 0) 
-        {
-            return;
-        }
-        this._saldo += valor;
-    }
+    this._saldo += valor;
 }
 
 const cliente1 = new Cliente();
