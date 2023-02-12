3c8fd45a32bc9d2ded14353de6ac6cd0367e4ae6
Jethro <jethrotiago@hotmail.com>
2023-02-11 22:52:33 -0300

    Organizado códigos de ContaCorreente.js

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,4 +1,20 @@
 class ContaCorrente {
     agencia;
     _saldo;
-}
+
+    sacar(valor) {
+        let valorSacado = 200;
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        };
+    };
+
+    depositar(valor) { 
+        if(valor <= 0) 
+        {
+            return;
+        }
+        this._saldo += valor;
+    };
+};
