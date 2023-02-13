d04851b2325544e483d111448d92957b6dbf96b0
Jethro <jethrotiago@hotmail.com>
2023-02-13 17:40:31 -0300

    Criado transferencia entre contas

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ cliente3.cpf = "02202202202";
 const conta1 = new ContaCorrente();
 conta1.agencia = 1001;
 conta1.cliente = cliente1;
+conta1.depositar(500);
 
 const conta2 = new ContaCorrente();
 conta2.agencia = 1001;
@@ -24,6 +25,8 @@ conta2.cliente = cliente2;
 const conta3 = new ContaCorrente();
 conta3.agencia = 200;
 conta3.cliente = cliente3;
+
+conta1.transferir(200, conta2);
 
 console.log(conta1);
 console.log(conta2);
