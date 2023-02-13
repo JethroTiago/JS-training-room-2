65f54bd06b199d9c00a0d885dba6314429233584
Jethro <jethrotiago@hotmail.com>
2023-02-13 19:04:57 -0300

    Criado Constructor para o conta corrente

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = "11122233309";
+const cliente1 = new Cliente("Ricardo", "11122233309");
+const cliente2 = new Cliente("Alice", "88822233309");
+const cliente3 = new Cliente("Jethro", "02202202202");
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = "88822233309";
-
-const cliente3 = new Cliente();
-cliente3.nome = "Jethro";
-cliente3.cpf = "02202202202";
-
-const conta1 = new ContaCorrente();
-conta1.agencia = 1001;
-conta1.cliente = cliente1;
+const conta1 = new ContaCorrente(1001, cliente1);
 conta1.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.agencia = 1001;
-conta2.cliente = cliente2;
+const conta2 = new ContaCorrente(1001, cliente2);
 
-const conta3 = new ContaCorrente();
-conta3.agencia = 200;
-conta3.cliente = cliente3;
+const conta3 = new ContaCorrente(200, cliente3);
 
 conta1.transferir(200, conta2);
 
